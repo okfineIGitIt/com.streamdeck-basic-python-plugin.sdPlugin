c76264a638fbca4285caf7ccd490bb2f8ebca144
Rehann John <rehannj@gmail.com>
2021-10-28 18:45:19 -0400

    Create plugin_core file with the base Plugin class that can be subclassed and used for setup and basic functions

--- com.streamdeck-python-plugin.sdPlugin/plugin/plugin_core.py
+++ com.streamdeck-python-plugin.sdPlugin/plugin/plugin_core.py
@@ -1,31 +1,19 @@
 import asyncio
 import json
 import logging
-from logging.handlers import RotatingFileHandler
-import re
-import sys
 import websockets
+
+import send_events as se
+
 
 LOCALHOST = "ws://localhost"
 
-
-logging.basicConfig(
-    handlers=[RotatingFileHandler('debug.log', maxBytes=100000, backupCount=10)],
-    level=logging.DEBUG,
-    format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
-    datefmt='%Y-%m-%dT%H:%M:%S'
-)
 
 class Plugin:
     """Plugin object to register Stream Deck plugin and call actions."""
 
     def __init__(
-        self, 
-        port, 
-        pluginUUID, 
-        registerEvent, 
-        info, 
-        loop,
+            self, port, pluginUUID, registerEvent, info, loop,
     ):
         """Plugin object initializer.
 
@@ -44,20 +32,56 @@ class Plugin:
         self.registerEvent = registerEvent
         self.info = info
         self.loop = loop
-        
-        self.sd_context = None  # context ID provided by Stream Deck once connected
-
-        self.state = 0  # Just here for testing purposes as of now
 
     def __del__(self):
+        """Cleanup websocket connections and async loop when plugin is deleted."""
         try:
             self.websocket.close()
             self.loop.close()
         except Exception as err:
             logging.critical(err)
 
+    async def set_title(self, *args, **kwargs):
+        """Set the button title in the Stream Deck App and/or Device."""
+        payload = se.create_set_title_payload(*args, **kwargs)
+        await self.send_message(json.dumps(payload))
+
+    async def set_image(self, *args, **kwargs):
+        """Set the button image in the Stream Deck App and/or Device."""
+        payload = se.create_set_image_payload(*args, **kwargs)
+        await self.send_message(json.dumps(payload))
+
+    async def show_alert(self, *args, **kwargs):
+        """Show an alert on the Stream Deck Device and Software."""
+        payload = se.create_show_alert_payload(*args, **kwargs)
+        await self.send_message(json.dumps(payload))
+
+    async def show_ok(self, *args, **kwargs):
+        """Show an 'ok' checkmark symbol in the Stream Deck App and/or Device."""
+        payload = se.create_show_ok_payload(*args, **kwargs)
+        await self.send_message(json.dumps(payload))
+
+    async def set_state(self, *args, **kwargs):
+        """Set the state of an action that supports multiple states."""
+        payload = se.create_set_state_payload(*args, **kwargs)
+        await self.send_message(json.dumps(payload))
+
+    async def switch_to_profile(self, *args, **kwargs):
+        """Switch to a Stream Deck profile."""
+        """Switch to """
+        payload = se.create_switch_to_profile_payload(*args, **kwargs)
+        await self.send_message(json.dumps(payload))
+
+    async def send_to_property_inspector(self, *args, **kwargs):
+        """Send a payload to the property inspector"""
+        payload = se.create_send_to_property_inspector_payload(
+            *args, **kwargs
+        )
+        await self.send_message(json.dumps(payload))
+
     async def listen(self):
         """listens to port provided by Stream Deck app and processes messages."""
+
         try:
             await self._init_websocket()
             await self._register_websocket()
@@ -76,7 +100,7 @@ class Plugin:
 
     async def _register_websocket(self):
         """Registers plugin with Stream Deck.
-        
+
         Uses `registerEvent` and pluginUUID` provided by the Stream Deck.
         """
         try:
@@ -91,11 +115,21 @@ class Plugin:
         except Exception as err:
             logging.critical(err)
 
+    async def on_streamdeck_message(self):
+        try:
+            while True:
+                message = await self.websocket.recv()
+                logging.debug("StreamDeck Message Received")
+                await self.process_data(json.loads(message))
+        except websockets.exceptions.ConnectionClosedOK:
+            logging.info("Stream Deck connection closed.")
+            self.loop.stop()
+
     async def on_message(self):
         """Waits for message from Stream Deck to send for processing."""
         try:
-            async for message in self.websocket:
-                self.process_data(json.loads(message))
+            task = asyncio.create_task(self.on_streamdeck_message())
+            await task
         except Exception as err:
             logging.critical(err)
 
@@ -106,12 +140,12 @@ class Plugin:
             event (str): Serialized json event object.
         """
         try:
-            logging.info(f"Sending event: {event}")
             await self.websocket.send(event)
         except Exception as err:
             logging.critical(err)
+            await self.show_alert()
 
-    def process_data(self, data):
+    async def process_data(self, data):
         """Process data and perform actions.
 
         Args:
@@ -119,65 +153,3 @@ class Plugin:
         """
 
         logging.info(f"Processing data: {data}")
-        self.state = self.state + 1
-        logging.info(f"Testing state: {self.state}")
-        
-        try:
-            if "payload" in data:
-                if self.sd_context is None:
-                    self.sd_context = data["context"]
-        except Exception as err:
-            logging.critical(err)
-
-
-def parse_args(sys_args):
-    """Parse arguments passed by Stream Deck app.
-    
-    Args:
-        sys_args (str): Argument string passed by Stream Deck.
-    """
-    args_length = len(sys_args)
-    args = {}
-    if args_length > 1:
-        try:
-            reg = re.compile('-(.*)')
-            for i in range(1, args_length, 2):
-                flag_index = i
-                value_index = i + 1
-                flag = reg.search(sys.argv[flag_index]).group(1)
-                value = sys.argv[value_index]
-                args[flag] = value
-                logging.info(f"Flag: {flag}, Value: {value}, Type: {type(value)}")
-        except Exception as err:
-            logging.critical(err)
-    return args
-
-def main():
-    try:
-        args = parse_args(sys.argv)
-        loop = asyncio.get_event_loop()
-
-        plugin = Plugin(
-            port=args['port'], 
-            pluginUUID=args['pluginUUID'],
-            registerEvent=args['registerEvent'], 
-            info=args['info'], 
-            loop=loop,
-        )
-
-        loop.run_until_complete(
-            asyncio.gather(
-                plugin.listen()
-            )
-        )
-
-        loop.run_forever()
-        loop.stop()
-        loop.close()
-    except Exception as err:
-        logging.critical(err)
-        loop.stop()
-        loop.close()
-
-if __name__ == '__main__':
-    main()
